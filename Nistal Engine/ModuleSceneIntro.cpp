14eeaa0a866dec913c13804dd04e0de13cfbb5ef
AlbertCayuela <albertcayuela16@gmail.com>
2020-09-28 19:38:19 +0200

    Help window working  + glew added

--- Nistal Engine/ModuleSceneIntro.cpp
+++ Nistal Engine/ModuleSceneIntro.cpp
@@ -3,10 +3,20 @@
 #include "ModuleSceneIntro.h"
 #include "Primitive.h"
 
+#include "ImGui/imconfig.h"
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_sdl.h"
+#include "ImGui/imgui_impl_opengl3.h"
+#include "Glew/include/glew.h"
+
+#include "SDL/include/SDL.h"
+#include "SDL/include/SDL_opengl.h"
+
+#pragma comment (lib, "Glew/lib/glew32.lib")
+
 
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
-{
-}
+{}
 
 ModuleSceneIntro::~ModuleSceneIntro()
 {}
@@ -14,34 +24,62 @@ ModuleSceneIntro::~ModuleSceneIntro()
 // Load assets
 bool ModuleSceneIntro::Start()
 {
-	LOG("Loading Intro assets");
-	bool ret = true;
+    LOG("Loading Intro assets");
+    bool ret = true;
 
-	//App->camera->Move(vec3(0.0f, 0.0f, 0.0f));
-	App->camera->LookAt(vec3(0, 0, 0));
+    //App->camera->Move(vec3(0.0f, 0.0f, 0.0f));
+    App->camera->LookAt(vec3(0, 0, 0));
 
-	return ret;
+    glewInit();
+    ImGui::CreateContext();
+    ImGui_ImplSDL2_InitForOpenGL(App->window->window, App->renderer3D->context);
+    ImGui_ImplOpenGL3_Init();
+    ImGui::StyleColorsDark();
+
+    return ret;
 }
 
-// Load assets
-bool ModuleSceneIntro::CleanUp()
+update_status ModuleSceneIntro::PreUpdate(float dt)
 {
-	LOG("Unloading Intro scene");
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplSDL2_NewFrame(App->window->window);
+    ImGui::NewFrame();
 
-	return true;
+    ImGui::ShowDemoWindow(&show_window);
+
+    return UPDATE_CONTINUE;
 }
 
 // Update
 update_status ModuleSceneIntro::Update(float dt)
 {
-	Plane p(0, 1, 0, 0);
-	p.axis = true;
-	p.Render();
+    Plane p(0, 1, 0, 0);
+    p.axis = true;
+    p.Render();
 
-	return UPDATE_CONTINUE;
+    return UPDATE_CONTINUE;
 }
 
-void ModuleSceneIntro::OnCollision(PhysBody3D* body1, PhysBody3D* body2)
+update_status ModuleSceneIntro::PostUpdate(float dt)
 {
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    return UPDATE_CONTINUE;
 }
 
+// Load assets
+bool ModuleSceneIntro::CleanUp()
+{
+    LOG("Unloading Intro scene");
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_GL_DeleteContext(App->renderer3D->context);
+    SDL_DestroyWindow(App->window->window);
+    SDL_Quit();
+
+    return true;
+}
