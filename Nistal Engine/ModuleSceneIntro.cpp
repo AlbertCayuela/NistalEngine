c748bb00d3f8665af284a58caaf3b790add37734
Nadine Gutiérrez <36292059+Nadine044@users.noreply.github.com>
2020-10-02 18:46:30 +0200

    PCG files added

--- Nistal Engine/ModuleSceneIntro.cpp
+++ Nistal Engine/ModuleSceneIntro.cpp
@@ -3,22 +3,6 @@
 #include "ModuleSceneIntro.h"
 #include "Primitive.h"
 
-//#include "ImGui/imconfig.h"
-//#include "ImGui/imgui.h"
-//#include "ImGui/imgui_impl_sdl.h"
-//#include "ImGui/imgui_impl_opengl3.h"
-//#include "Glew/include/glew.h"
-//
-//#include "SDL/include/SDL.h"
-//#include "SDL/include/SDL_opengl.h"
-//
-//#pragma comment (lib, "Glew/lib/glew32.lib")
-//#ifdef NDEBUG
-//#pragma comment (lib, "MathGeoLib/libx86/Release/MathGeoLib.lib") 
-//#else
-//#pragma comment (lib, "MathGeoLib/libx86/Debug/MathGeoLib.lib") 
-//#endif
-
 
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
 {}
@@ -26,7 +10,6 @@ ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Modul
 ModuleSceneIntro::~ModuleSceneIntro()
 {}
 
-// Load assets
 bool ModuleSceneIntro::Start()
 {
     LOG("Loading Intro assets");
@@ -35,23 +18,11 @@ bool ModuleSceneIntro::Start()
     //App->camera->Move(vec3(0.0f, 0.0f, 0.0f));
     App->camera->LookAt(vec3(0, 0, 0));
 
-    //glewInit();
-    //ImGui::CreateContext();
-    //ImGui_ImplSDL2_InitForOpenGL(App->window->window, App->renderer3D->context);
-    //ImGui_ImplOpenGL3_Init();
-    //ImGui::StyleColorsDark();
-
     return ret;
 }
 
 update_status ModuleSceneIntro::PreUpdate(float dt)
 {
-    //ImGui_ImplOpenGL3_NewFrame();
-    //ImGui_ImplSDL2_NewFrame(App->window->window);
-    //ImGui::NewFrame();
-
-    //ImGui::ShowDemoWindow(&show_window);
-
     return UPDATE_CONTINUE;
 }
 
@@ -62,29 +33,12 @@ update_status ModuleSceneIntro::Update(float dt)
     p.axis = true;
     p.Render();
 
-    //CLOSING WINDOW
-
-    //math::Sphere x({ 0,0,0 }, 4);
-    //math::Sphere y({ 0,0,0 }, 7);
-
-    //if (x.Intersects(y))
-    //    LOG("BEEP BEEP!!");
-    //
-    //ImGui::Text("Closing Window");
-    //if (ImGui::Button("Exit"))
-    //{
-    //    return UPDATE_STOP;
-    //}
-
 
     return UPDATE_CONTINUE;
 }
 
 update_status ModuleSceneIntro::PostUpdate(float dt)
 {
-    //ImGui::Render();
-    //ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
     return UPDATE_CONTINUE;
 }
 
@@ -92,14 +46,6 @@ update_status ModuleSceneIntro::PostUpdate(float dt)
 bool ModuleSceneIntro::CleanUp()
 {
     LOG("Unloading Intro scene");
-
- /*   ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplSDL2_Shutdown();
-    ImGui::DestroyContext();
-
-    SDL_GL_DeleteContext(App->renderer3D->context);
-    SDL_DestroyWindow(App->window->window);
-    SDL_Quit();*/
 
     return true;
 }
