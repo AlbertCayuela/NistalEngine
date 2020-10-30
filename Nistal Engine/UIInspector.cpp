a523feb737a8500d033e16a46cbd66f23412ad57
Nadine Gutiérrez <36292059+Nadine044@users.noreply.github.com>
2020-10-30 12:11:16 +0100

    Loading some data in Inspector

--- Nistal Engine/UIInspector.cpp
+++ Nistal Engine/UIInspector.cpp
@@ -35,6 +35,46 @@ void UIInspector::Draw()
 {
 	if (Begin("Inspector", &is_on, 0))
 	{
+		if (CollapsingHeader("Transform"))
+		{
+			/*static float col1[3] = { 1.0f, 0.0f, 0.2f };
+			static float col2[4] = { 0.4f, 0.7f, 0.0f, 0.5f };
+			ImGui::ColorEdit3("color 1", col1);
+			ImGui::ColorEdit4("color 2", col2);	*/	
+
+			Text("Game Object name:");
+			Separator();
+
+			Text("Position:"); SameLine();
+			static float position[4] = { 0.0f, 0.0f, 0.0f };
+			ImGui::InputFloat3("", position);
+
+			Text("Rotation:"); SameLine();
+			static float rotation[4] = { 0.0f, 0.0f, 0.0f };
+			ImGui::InputFloat3("", rotation);
+
+			Text("Scale:"); SameLine();
+			static float scale[4] = { 1.0f, 1.0f, 1.0f };
+			ImGui::InputFloat3("", scale);
+
+			static int world = 0;
+			ImGui::RadioButton("world", &world, 2); ImGui::SameLine();
+			static int local = 0;
+			ImGui::RadioButton("local", &local, 2);
+		}
+		if (CollapsingHeader("Geometry Mesh"))
+		{
+			Text("Number of vertices: %i", App->load_fbx->model.num_vertex);
+			Text("Number of indices: %i", App->load_fbx->model.num_index);
+			Text("Number of faces: %i", App->load_fbx->model.num_vertex / 3);
+			//Text("Number of normals: %i", App->load_fbx->model.num_normals);
+			Text("Number of UVs: %i", App->load_fbx->model.num_uvs);
+		}
+		if (CollapsingHeader("Material"))
+		{
+			//hellow :)
+		}
+
 		LOG("Hellow");
 	}
 	End();
