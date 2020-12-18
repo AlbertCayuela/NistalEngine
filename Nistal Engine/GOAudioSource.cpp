7b6f60e3dd19bee7dd98f87c1133b4e6409cf546
AlbertCayuela <albertcayuela16@gmail.com>
2020-12-18 10:34:06 +0100

    Added Component Audio Source files

---
+++ Nistal Engine/GOAudioSource.cpp
@@ -0,0 +1,10 @@
+#include "GOAudioSource.h"
+#include "Application.h"
+
+GOAudioSource::GOAudioSource(GameObject* parent) : GOComponent(parent)
+{
+}
+
+GOAudioSource::~GOAudioSource()
+{
+}
---
+++ Nistal Engine/GOAudioSource.h
@@ -0,0 +1,14 @@
+#ifndef __GOAUDIOSOURCE_H__
+#define __GOAUDIOSOURCE_H__
+
+#include "GOComponent.h"
+
+class GOAudioSource : public GOComponent 
+{
+public:
+
+	GOAudioSource(GameObject* parent);
+	~GOAudioSource();
+};
+
+#endif
