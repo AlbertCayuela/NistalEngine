2cdf9fd97df3ff65bb6216a9d41ecfe970640165
AlbertCayuela <albertcayuela16@gmail.com>
2020-12-18 10:38:55 +0100

    Added component audio listener files

---
+++ Nistal Engine/GOAudioListener.cpp
@@ -0,0 +1,11 @@
+#include "GOAudioListener.h"
+#include "Application.h"
+
+GOAudioListener::GOAudioListener(GameObject* parent) : GOComponent(parent)
+{
+
+}
+
+GOAudioListener::~GOAudioListener()
+{
+}
---
+++ Nistal Engine/GOAudioListener.h
@@ -0,0 +1,14 @@
+#ifndef __GOAUDIOLISTENER_H__
+#define __GOAUDIOLISTENER_H__
+
+#include "GOComponent.h"
+
+class GOAudioListener : public GOComponent
+{
+public:
+
+	GOAudioListener(GameObject* parent);
+	~GOAudioListener();
+};
+
+#endif
