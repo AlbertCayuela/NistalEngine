dc72bf34c2077c3fa2e1c7e4977540b1211c6347
AlbertCayuela <albertcayuela16@gmail.com>
2021-01-01 15:52:39 +0100

    first song plays (sound is not good)

--- Nistal Engine/ModuleAudio.cpp
+++ Nistal Engine/ModuleAudio.cpp
@@ -2,25 +2,58 @@
 #include "Application.h"
 #include "ModuleAudio.h"
 
+using namespace WwiseT;
+
 ModuleAudio::ModuleAudio(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
 }
 
 ModuleAudio::~ModuleAudio()
 {
+	
 }
 
 bool ModuleAudio::Start()
 {
+	InitSoundEngine();
+
+	LoadBank("Engines3.bnk");
+
+	source = CreateSoundSource("mysource");
+	SetListener(source);
+
 	return true;
 }
 
 update_status ModuleAudio::Update(float dt)
 {
+	source->PlayEventByName("PlaySong1");
+
+	ProcessAudio();
+
 	return UPDATE_CONTINUE;
 }
 
 bool ModuleAudio::CleanUp()
 {
-	return true;;
+	CloseSoundEngine();
+
+	return true;
+}
+
+AudioSource* ModuleAudio::CreateSoundSource(const char* source_name)
+{
+	AudioSource* new_source = CreateAudSource(source_name);
+
+	return new_source;
+}
+
+bool ModuleAudio::SetListener(AudioSource* listener)
+{
+	bool ret = true;
+
+	this->listener = listener;
+	SetDefaultListener(listener->GetID());
+
+	return ret;
 }
--- Nistal Engine/ModuleAudio.h
+++ Nistal Engine/ModuleAudio.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "Module.h"
 #include "Globals.h"
+#include "WwiseT.h"
 
+using namespace WwiseT;
 
 class ModuleAudio : public Module
 {
@@ -12,4 +14,12 @@ public:
 	bool Start();
 	update_status Update(float dt);
 	bool CleanUp();
+
+	AudioSource* CreateSoundSource(const char* source_name);
+	bool SetListener(AudioSource* listener);
+
+protected:
+
+	AudioSource* listener;
+	AudioSource* source;
 };
